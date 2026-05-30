8969090c13ef69f345f15edf61e7352087da6c24
boby <145615559+bobychong@users.noreply.github.com>
2026-05-30 18:31:20 +0800

    Create signin.py

---
+++ signin.py
@@ -0,0 +1,22 @@
+import os
+import random
+import time
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+
+API_ID = int(os.environ['API_ID'])
+API_HASH = os.environ['API_HASH']
+SESSION = os.environ['SESSION']
+GROUP1 = os.environ['GROUP1']
+GROUP2 = os.environ['GROUP2']
+
+delay = 0
+
+with TelegramClient(StringSession(SESSION), API_ID, API_HASH) as client:
+    client.send_message(GROUP1, '/qd')
+    print(f"{GROUP1} 签到成功！")
+    
+    time.sleep(random.randint(10, 60))
+    
+    client.send_message(GROUP2, '/sign')
+    print(f"{GROUP2} 签到成功！")
